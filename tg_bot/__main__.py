89c5abb45270666ab206563482d48147c76ae9c3
Hasher-spade <61341934+Hasher-spade@users.noreply.github.com>
2020-02-28 11:42:37 +0530

    Hu

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -17,19 +17,13 @@ from tg_bot.modules import ALL_MODULES
 from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
-PM_START_TEXT = """
-hoi {}, my name is {}! if you have any questions about how to use me please give me /help... 
+PM_START_TEXT = """ 
+Hi {} ! Thanks for using {}
+I am special bot created by Coisnto Ever(tg://user?id={})
 
-im a group manager bot maintained by  [this person](tg://user?id={}).
-
-My future updates will be put into This Channel - @MarieChechi & My Support Group @InFoTelGroup.
-
-This is my [Deploy Code](https://heroku.com/deploy?template=https://github.com/TGExplore/Marie-2.0-English),
-you can create clone same like me..
-
-For more commands click /help...
-
-**Keep in mind that any changes you DO do to the source have to be on github, as per the license.**
+I won't reply to your messages here or work for you 
+So Don't waste your precious time with Me 😁
+Coinsto Community (buttonurl:http://t.me/coinstocommunity)
 
 """
 
